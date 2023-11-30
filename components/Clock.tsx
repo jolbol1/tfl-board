4d73f323f035a8e6b975cb0d37e84fe94adff86b
jolbol1 <github@jamesshopland.com>
2023-11-30 04:11:12 +0000

    initial clock value

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -3,20 +3,25 @@
 import { cn } from "@/lib/utils";
 import React, { useState, useEffect } from "react";
 
+export const getTime = () => {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  return `${hours}:${minutes}:${seconds}`;
+};
+
 export const Clock = ({
   variant = "old",
   ...props
 }: React.HTMLAttributes<HTMLDivElement> & { variant: "old" | "new" }) => {
-  const [time, setTime] = useState("");
+  const [time, setTime] = useState("00:00:00");
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      const now = new Date();
-      const hours = String(now.getHours()).padStart(2, "0");
-      const minutes = String(now.getMinutes()).padStart(2, "0");
-      const seconds = String(now.getSeconds()).padStart(2, "0");
+    setTime(getTime());
 
-      setTime(`${hours}:${minutes}:${seconds}`);
+    const interval = setInterval(() => {
+      setTime(getTime());
     }, 1000);
 
     return () => clearInterval(interval);
